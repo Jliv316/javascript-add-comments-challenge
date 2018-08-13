ce28a7fc643a820c1a8fd0a93474e588271b5030
Jliv316 <jclivingston316@gmail.com>
2018-08-13 07:35:20 -0600

    first commit

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
   // add your code here
+  const newCommentButton = $("#new_comment_button");
+  let commentList = $('#comment_list');
+  const newCommentForm = $("#new_comment_form");
+  newCommentForm.hide();
+
+  newCommentButton.on('click', () => {
+    newCommentButton.hide();
+    newCommentForm.show();
+  });
+
+  newCommentForm.submit(() => {
+    event.preventDefault();
+
+    commentList.append('<li>' + $('textarea[name="comment"]').val() + "<div class='author'>" + $('input[id="author"]').val() + "</div></li>");
+
+    newCommentButton.show();
+    newCommentForm.hide();
+  });
 })
